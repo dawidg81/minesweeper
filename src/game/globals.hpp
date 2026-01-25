67f48505819520f057ec933721a52a85868ee88f
dawidg81 <dawidgorski.m@gmail.com>
2026-01-25 15:09:34 +0100

    experimental builds and header fix

--- src/game/globals.hpp
+++ src/game/globals.hpp
@@ -1,7 +1,25 @@
 #ifndef GAME_HPP
 #define GAME_HPP
 
-extern bool inGame;
-void game(int diff);
+#include <iostream>
+
+class Game {
+private:
+    static constexpr int MAX_W = 32;
+    static constexpr int MAX_H = 32;
+
+    int boardWidth;
+    int boardHeight;
+
+    bool bombMap[MAX_H][MAX_W];
+    int tileMap[MAX_H][MAX_W];
+
+    int mines;
+
+public:
+    void initDiff(int diff);
+    void initBoard();
+    void displayBoard();
+};
 
 #endif
