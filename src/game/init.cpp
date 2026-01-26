11e83f577039ec03403720b416e7f8bd66606342
dawidg81 <dawidgorski.m@gmail.com>
2026-01-26 16:21:15 +0100

    fixed syntax

--- src/game/init.cpp
+++ src/game/init.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "Game.hpp"
 
 void Game::initDiff(int diff) {
@@ -5,22 +7,20 @@ void Game::initDiff(int diff) {
         case 0:
             boardWidth  = 9;
             boardHeight = 9;
-                                                    mines       = 9;
-                                                                break;
-                                                                        case 1:
-                                                                                    boardWidth  = 16;
-                                                                                                boardHeight = 16;
-                                                                                                            mines       = 12;
-                                                                                                                        break;
-                                                                                                                                case 2:
-                                                                                                                                            boardWidth  = 30;
-                                                                                                                                                        boardHeight = 16;
-                                                                                                                                                                    mines       = 16;
-                                                                                                                                                                                break;
-                                                                                                                                                                                        case 3:
-                                                                                                                                                                                                    std::cout << "Editing game parameters\n";
-                                                                                                                                                                                                                break;
-                                                                                                                                                                                                                    }
-                                                                                                                                                                                                                    }
-
+            mines       = 9;
+            break;
+        case 1:
+            boardWidth  = 16;
+            boardHeight = 16;
+            mines       = 12;
+            break;
+        case 2:
+            boardWidth  = 30;
+            boardHeight = 16;
+            mines       = 16;
+            break;
+        case 3:
+            std::cout << "Editing game parameters\n";
+            break;
+        }
 }
