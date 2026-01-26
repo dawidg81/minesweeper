431a3d5a7da43787550850edc5cd8be25c6d76ba
dawidg81 <dawidgorski.m@gmail.com>
2026-01-26 16:02:31 +0100

    code fragmentation

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -10,29 +10,6 @@ void Game::editDiff(){
     cout << "Now editing custom difficulty" << endl;
 }
 
-void Game::initDiff(int diff) {
-    switch (diff) {
-        case 0:
-            boardWidth  = 9;
-            boardHeight = 9;
-            mines       = 9;
-            break;
-        case 1:
-            boardWidth  = 16;
-            boardHeight = 16;
-            mines       = 12;
-            break;
-        case 2:
-            boardWidth  = 30;
-            boardHeight = 16;
-            mines       = 16;
-            break;
-        case 3:
-            std::cout << "Editing game parameters\n";
-            break;
-    }
-}
-
 void Game::initBoard() {
     for (int i = 0; i < boardHeight; i++)
         for (int j = 0; j < boardWidth; j++)
---
+++ src/game/init.cpp
@@ -0,0 +1,26 @@
+#include "Game.hpp"
+
+void Game::initDiff(int diff) {
+    switch (diff) {
+        case 0:
+            boardWidth  = 9;
+            boardHeight = 9;
+                                                    mines       = 9;
+                                                                break;
+                                                                        case 1:
+                                                                                    boardWidth  = 16;
+                                                                                                boardHeight = 16;
+                                                                                                            mines       = 12;
+                                                                                                                        break;
+                                                                                                                                case 2:
+                                                                                                                                            boardWidth  = 30;
+                                                                                                                                                        boardHeight = 16;
+                                                                                                                                                                    mines       = 16;
+                                                                                                                                                                                break;
+                                                                                                                                                                                        case 3:
+                                                                                                                                                                                                    std::cout << "Editing game parameters\n";
+                                                                                                                                                                                                                break;
+                                                                                                                                                                                                                    }
+                                                                                                                                                                                                                    }
+
+}
