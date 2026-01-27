f34e07b4962f65362afa5ca98759e87929d964bc
dawidg81 <dawidgorski.m@gmail.com>
2026-01-27 19:19:30 +0100

    file node

--- src/Utils/catch.cpp
+++ src/Utils/catch.cpp
@@ -3,6 +3,17 @@
 #include <iostream>
 #include <ostream>
 
+int Utils::catchInputInt(int* input){
+    std::cin >> input;
+    
+    if (std::cin.fail()) {
+        std::cout << "error: Input failed" << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
+
 int Utils::catchReturn(int renum){
     if (renum != 0) {
         std::cout << "Received error signal, sending further" << std::endl;
