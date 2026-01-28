4b27e7dc8fc6e540a3b1b9578592d07911a38237
dawidg81 <dawidgorski.m@gmail.com>
2026-01-28 18:14:21 +0100

    merging Game class implementation into one file

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -5,3 +5,40 @@
 #include <cstdio>
 #include <iostream>
 #include <string>
+
+void Game::initDiff(int diff) {
+    switch (diff) {
+        case 0:
+            boardWidth  = 9;
+            boardHeight = 9;
+            mines       = 9;
+            break;
+        case 1:
+            boardWidth  = 16;
+            boardHeight = 16;
+            mines       = 12;
+            break;
+        case 2:
+            boardWidth  = 30;
+            boardHeight = 16;
+            mines       = 16;
+            break;
+        case 3:
+            std::cout << "Editing game parameters\n";
+            break;
+    }
+}
+
+void Game::initBoard() {
+    for (int i = 0; i < boardHeight; i++) {
+        for (int j = 0; j < boardWidth; j++) {
+            bombMap[i][j] = rand() % 2;
+        }
+    }
+
+    for (int i = 0; i < boardHeight; i++) {
+        for (int j = 0; j < boardWidth; j++) {
+            tileMap[i][j] = 0;
+        }
+    }
+}
