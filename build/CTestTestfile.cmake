8c00822555b0bf8a3aa0c3d8e6e5400c0cb6d820
dawidg81 <dawidgorski.m@gmail.com>
2026-02-01 22:10:13 +0100

    The project is no longer a CMake project. Makefile will be used in the future.

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dawidg81/minesweeper
-# Build directory: /home/dawidg81/minesweeper/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(runs_minesweeper "/home/dawidg81/minesweeper/build/minesweeper")
-set_tests_properties(runs_minesweeper PROPERTIES  _BACKTRACE_TRIPLES "/home/dawidg81/minesweeper/CMakeLists.txt;19;add_test;/home/dawidg81/minesweeper/CMakeLists.txt;0;")
