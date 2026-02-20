93dfba58372d0266972bbe0448eb9f3d09bf2b72
dawidg81 <dawidgorski.m@gmail.com>
2026-02-20 15:40:34 +0100

    added colors

---
+++ src/colors.cpp
@@ -0,0 +1,43 @@
+#include "colors.hpp"
+
+#include <cstdlib>
+#include <iostream>
+
+void changeColorWin(int col) {
+    switch (col) {
+        case 1:
+        std::system("color 2");
+        break;
+
+        case 2:
+        std::system("color 2");
+        break;
+
+        case 3:
+        std::system("color 2");
+        break;
+
+        case 4:
+        std::system("color 2");
+        break;
+
+        case 5:
+        std::system("color 2");
+        break;
+        
+        case 6:
+        std::system("color 2");
+        break;
+
+        case 7:
+        std::system("color 2");
+        break;
+    }
+}
+
+void changeColor(int col) {
+    switch (col) {
+    
+    }
+    std::cout << COL_GREEN
+}
--- src/main.cpp
+++ src/main.cpp
@@ -5,48 +5,47 @@
 
 using namespace std;
 
-int main(){
+int main() {
 
-	Game game;
-	game.inGame = false;
-	int choice;
+  Game game;
+  game.inGame = false;
+  int choice;
 
-	cout << "Welcome to minesweeper v0.2.0\n";
-	cout << "\n";
-	cout << "Select difficulty:\n"
-		"1. Easy\n"
-		"2. Medium\n"
-		"3. Hard\n"
-		"4. Custom\n";
-	cout << "Difficulty: ";
-	cin >> choice;
+  cout << "Welcome to minesweeper v0.2.0\n";
+  cout << "\n";
+  cout << "Select difficulty:\n"
+          "1. Easy\n"
+          "2. Medium\n"
+          "3. Hard\n"
+          "4. Custom\n";
+  cout << "Difficulty: ";
+  cin >> choice;
 
-	if(choice == 1){
-		game.initDiff(0);
-		game.inGame = true;
-	} else if(choice == 2){
-		game.initDiff(1);
-		game.inGame = true;
-	} else if(choice == 3){
-		game.initDiff(2);
-		game.inGame = true;
-	} else if(choice == 4){
-		game.editDiff();
-		game.inGame = true;
-	}
+  if (choice == 1) {
+    game.initDiff(0);
+    game.inGame = true;
+  } else if (choice == 2) {
+    game.initDiff(1);
+    game.inGame = true;
+  } else if (choice == 3) {
+    game.initDiff(2);
+    game.inGame = true;
+  } else if (choice == 4) {
+    game.editDiff();
+    game.inGame = true;
+  }
 
-	srand(time(nullptr));
-	game.initBoard();
+  srand(time(nullptr));
+  game.initBoard();
 
-	while(game.inGame){
-		game.displayBoard();
-		game.input();
-		game.updateBoard();
-		game.hasWon();
-		// cout << game.msg;
-		// cout << game.inGame;
-	}
+  while (game.inGame) {
+    game.displayBoard();
+    game.input();
+    game.updateBoard();
+    game.hasWon();
+    // cout << game.msg;
+    // cout << game.inGame;
+  }
 
-	return 0;
-	
+  return 0;
 }
